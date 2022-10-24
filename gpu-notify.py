9af692aaadba5569b3964e3e1828e32b50838ed5
methanon46 <tougey.s@gmail.com>
2022-10-24 19:02:25 +0900

    honban

--- gpu-notify.py
+++ gpu-notify.py
@@ -38,9 +38,9 @@ if __name__ == '__main__':
                         pysmi = nvidia_smi.getInstance().DeviceQuery()["gpu"][0]["processes"]["pid"==i["pid"]]
                         # pid -> ユーザー名はpsコマンド以外で取得する術を知りませんでした
                         ps = subprocess.run("ps axo user,pid | grep {}".format(i["pid"]),capture_output=True,encoding="utf-8", shell=True).stdout[:-7]
-                        # jsonの改行は\\nです！！
+                        # 全然改行してくれない
                         json_data = {
-                            'text': 'Hi, {}\\npid:{}, {}MiB process remains without computing.\\nPlease shutdown your notebook or process.'.format(ps,i["pid"],pysmi["used_memory"]),
+                            'text': 'Hi, {}\npid:{}, {}MiB process remains without computing.\nPlease shutdown your notebook or process.'.format(ps,i["pid"],pysmi["used_memory"]),
                         }
                         response = requests.post(WEBHOOK_URL, headers=headers, json=json_data)
                         pids[i["pid"]]=[time.perf_counter(),1]
